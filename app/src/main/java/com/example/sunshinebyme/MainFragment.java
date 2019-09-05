d5fbfc59fd310874b602f4969c3f918be3e15407
hung-nv <hung.nv@teko.vn>
2019-09-05 21:22:20 +0700

    Fetching weather data

--- app/src/main/java/com/example/sunshinebyme/MainFragment.java
+++ app/src/main/java/com/example/sunshinebyme/MainFragment.java
@@ -1,21 +1,33 @@
 package com.example.sunshinebyme;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainFragment extends Fragment {
     private ListView listView;
     private WeatherAdapter adapter;
+    private TextView tvResult;
 
 
     @Nullable
@@ -24,9 +36,13 @@ public class MainFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_activity, container, false);
 
         listView = view.findViewById(R.id.listView);
+        tvResult = view.findViewById(R.id.tvResult);
         adapter = new WeatherAdapter(getContext());
         adapter.setData(getDummyWeathers());
         listView.setAdapter(adapter);
+
+        new WeatherTask().execute();
+
         return view;
     }
 
@@ -54,6 +70,41 @@ public class MainFragment extends Fragment {
         weathers.add(new Weather(R.drawable.ic_rain_24dp, "Thursday", "Rainly", 26, 16));
         weathers.add(new Weather(R.drawable.ic_rain_24dp, "Thursday", "Rainly", 26, 16));
         return weathers;
+    }
+
+    class WeatherTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            String result = "";
+            String baseURl = "https://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&appid=f99f7329e28b995c7bfd1a33f191b59b";
+            try {
+                URL url = new URL(baseURl);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            tvResult.setText(s);
+        }
     }
 
 }
